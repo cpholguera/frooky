ba524f4202d865def2cad1d5c72e42a2817ef6f5
Carlos Holguera <perezholguera@gmail.com>
2026-01-23 09:59:09 +0100

    Use a PEP 440 compliant local version

--- frooky/__init__.py
+++ frooky/__init__.py
@@ -3,6 +3,6 @@
 try:
     from ._version import version as __version__
 except ImportError:
-    __version__ = "unknown"
+    __version__ = "0+unknown" # Using a PEP 440 compliant local version to make it obvious this is not a real release.
 
 __all__ = ["__version__"]
