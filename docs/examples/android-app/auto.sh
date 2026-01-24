cf6953c71055ef504065e71f26f54c14478691b9
Carlos Holguera <perezholguera@gmail.com>
2026-01-24 11:16:53 +0100

    retry frooky

--- docs/examples/android-app/auto.sh
+++ docs/examples/android-app/auto.sh
@@ -18,25 +18,26 @@ frida-ps -Uai | grep -i mas
 PID="$(frida-ps -Uai | awk '$3=="org.owasp.mastestapp"{print $1; exit}')"
 echo "Target pid, $PID"
 
-set +e
-timeout 5s frida -U -p "$PID" -l frida_sanity.js
-RC=$?
-set -e
-echo "frida exit code, $RC"
+# set +e
+# timeout 5s frida -U -p "$PID" -l frida_sanity.js
+# RC=$?
+# set -e
+# echo "frida exit code, $RC"
 
 
 # Start frooky and redirect stdout and stderr to file
 # frooky -U -f org.owasp.mastestapp --platform android hooks.json hooks2.json --keep-artifacts -o "$OUTPUT_JSON" >"$FROOKY_LOG" 2>&1 &
+frooky -U -p "$PID" --platform android hooks.json hooks2.json --keep-artifacts -o "$OUTPUT_JSON" >"$FROOKY_LOG" 2>&1 &
 
-# FROOKY_PID=$!
+FROOKY_PID=$!
 
 # Run Maestro (https://docs.maestro.dev/getting-started/installing-maestro)
 maestro test "$FLOW" > auto.log 2>&1
 MAESTRO_EXIT=$?
 
 # Stop frooky when Maestro completes
-# kill -INT "$FROOKY_PID" 2>/dev/null || true
-# wait "$FROOKY_PID" 2>/dev/null || true
-# ls -laR .
+kill -INT "$FROOKY_PID" 2>/dev/null || true
+wait "$FROOKY_PID" 2>/dev/null || true
+ls -laR .
 
 exit "$MAESTRO_EXIT"
