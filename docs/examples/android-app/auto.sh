237f70da7abac84b1f49350834cc45fee786a5d7
Carlos Holguera <perezholguera@gmail.com>
2026-01-24 12:12:53 +0100

    run frooky in bg

--- docs/examples/android-app/auto.sh
+++ docs/examples/android-app/auto.sh
@@ -35,12 +35,15 @@ fi
 # Start frooky and redirect stdout and stderr to file
 # frooky -U -f org.owasp.mastestapp --platform android hooks.json hooks2.json --keep-artifacts -o "$OUTPUT_JSON" >"$FROOKY_LOG" 2>&1 &
 
-set +e
-timeout 5s frooky -U -p "$PID" --platform android hooks.json hooks2.json --keep-artifacts -o "$OUTPUT_JSON" >"$FROOKY_LOG" 2>&1
-RC=$?
-set -e
-echo "frooky exit code, $RC"
-tail -n 200 "$FROOKY_LOG" || true
+# set +e
+# timeout 5s frooky -U -p "$PID" --platform android hooks.json hooks2.json --keep-artifacts -o "$OUTPUT_JSON" >"$FROOKY_LOG" 2>&1
+# RC=$?
+# set -e
+# echo "frooky exit code, $RC"
+
+
+frooky -U -p "$PID" --platform android hooks.json hooks2.json --keep-artifacts -o "$OUTPUT_JSON" >>"$FROOKY_LOG" 2>&1 &
+FROOKY_PID=$!
 
 # set +e
 # timeout 5s frooky -U -p "$PID" --platform android hooks.json hooks2.json --keep-artifacts -o "$OUTPUT_JSON" >"$FROOKY_LOG" 2>&1
