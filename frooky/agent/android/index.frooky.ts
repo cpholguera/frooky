7cb0f063b9325811623b085421e83ca5a95bf7cb
bernhste <s.bernhardsgruetter@proton.me>
2026-01-25 12:40:14 +0100

    added explanatory comment to the new index.ts files

--- frooky/agent/android/index.frooky.ts
+++ frooky/agent/android/index.frooky.ts
@@ -1,3 +1,5 @@
+// This file is used when the agent is run by frooky. Hooks are dynamically loaded using rpc at runtime.
+
 import { runFrookyAgent } from './android-agent.ts'
 
 rpc.exports = {
--- frooky/agent/ios/index.frooky.ts
+++ frooky/agent/ios/index.frooky.ts
@@ -1,3 +1,5 @@
+// This file is used when the agent is run by frooky. Hooks are dynamically loaded using rpc at runtime.
+
 import { runFrookyAgent } from './ios-agent.ts'
 
 rpc.exports = {
