4602e6e5388681209870d606f0a17eea4c3a71dc
bernhste <s.bernhardsgruetter@proton.me>
2026-01-31 15:28:17 +0100

    init an example file, changed a comment, made MAS category optional

---
+++ frooky/agent/types/examples.ts
@@ -0,0 +1,13 @@
+/**
+ * This file contains examples of frooky hooks based in the defined API spec
+ * and their declaration in YAML. 
+ * 
+ * The point is to test them here how they
+ * look and work in practice. 
+ * 
+ * This the examples could then be moved to 
+ * the documentation or examples/ folder
+ */
+
+
+
