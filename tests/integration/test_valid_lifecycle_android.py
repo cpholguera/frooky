327491767caa9571ab706cc4ffad71a649f088af
bernhste <s.bernhardsgruetter@proton.me>
2026-02-03 23:36:41 +0100

    better naming

--- tests/integration/test_valid_lifecycle_android.py
+++ tests/integration/test_valid_lifecycle_android.py
@@ -5,20 +5,20 @@ from helpers.android_test_helpers import AndroidTestHelper
 class TestHookJavaMethod:
     """Tests for handling errors on the target related to Java methods."""
 
-    def test_hook_single_java_method(self, pid, hooks_dir, maestro_flow_path):
+    def test_hook_java_single_method(self, pid, hooks_dir, maestro_flow_path):
         """Test hooking a single Java method in a real process."""
         AndroidTestHelper.run_hook_test(
-            hooks_dir / "single_java_method.json",
+            hooks_dir / "java_single_method.json",
             "android.app.SharedPreferencesImpl$EditorImpl",
             ["putString"],
             pid,
             maestro_flow_path
         )
 
-    def test_hook_multiple_java_methods(self, pid, hooks_dir, maestro_flow_path):
+    def test_hook_java_multiple_methods(self, pid, hooks_dir, maestro_flow_path):
         """Test hooking multiple Java methods in a real process."""
         AndroidTestHelper.run_hook_test(
-            hooks_dir / "multiple_java_methods.json",
+            hooks_dir / "java_multiple_methods.json",
             "androidx.security.crypto.EncryptedSharedPreferences$Editor",
             ["putString", "putStringSet"],
             pid,
