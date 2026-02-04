a99b61471065e11af6e175b0c7162b4136e59e39
bernhste <s.bernhardsgruetter@proton.me>
2026-02-04 07:57:18 +0100

    added subset pattern matcher.

--- tests/integration/test_valid_lifecycle_android.py
+++ tests/integration/test_valid_lifecycle_android.py
@@ -4,40 +4,76 @@ from conftest import run_hook_test
 class TestHookJavaMethod:
     """Tests for handling errors on the target related to Java methods."""
 
-    def test_hook_java_single_method(self, pid, hooks_dir, maestro_flow_path):
-        """Test hooking a single Java method in a real process."""
+    # def test_hook_java_single_method(self, pid, hooks_dir, maestro_flow_path):
+    #     """Test hooking a single Java method in a real process."""
+
+    #     target_patterns = [
+    #         {
+    #             "class": "android.app.SharedPreferencesImpl$EditorImpl",
+    #             "method": "putString",
+    #         }
+    #     ]
+
+    #     run_hook_test(
+    #         hooks_dir / "java_single_method.json",
+    #         target_patterns,
+    #         pid,
+    #         maestro_flow_path,
+    #         "android"
+    #     )
+
+    # def test_hook_java_multiple_methods(self, pid, hooks_dir, maestro_flow_path):
+    #     """Test hooking multiple Java methods in a real process."""
+
+    #     target_patterns = [
+    #         {
+    #             "class": "androidx.security.crypto.EncryptedSharedPreferences$Editor",
+    #             "method": "putString",
+    #         },
+    #         {
+    #             "class": "androidx.security.crypto.EncryptedSharedPreferences$Editor",
+    #             "method": "putStringSet",
+    #         },
+    #     ]
+
+    #     run_hook_test(
+    #         hooks_dir / "java_multiple_methods.json",
+    #         target_patterns,
+    #         pid,
+    #         maestro_flow_path,
+    #         "android"
+    #     )
+
+
+    def test_hook_java_single_method_overload(self, pid, hooks_dir, maestro_flow_path):
+        """Test hooking single Java methods and one overload."""
 
         target_patterns = [
             {
-                "class": "android.app.SharedPreferencesImpl$EditorImpl",
-                "method": "putString",
+                "class": "androidx.security.crypto.EncryptedSharedPreferences",
+                "method": "create",
+                "inputParameters": [
+                        {
+                            "declaredType": "android.content.Context"
+                        },
+                        {
+                            "declaredType": "java.lang.String"
+                        },
+                        {
+                            "declaredType": "androidx.security.crypto.MasterKey"
+                        },
+                        {
+                            "declaredType": "androidx.security.crypto.EncryptedSharedPreferences$PrefKeyEncryptionScheme"
+                        },
+                        {
+                            "declaredType": "androidx.security.crypto.EncryptedSharedPreferences$PrefValueEncryptionScheme"
+                        }
+                    ],
             }
         ]
 
         run_hook_test(
-            hooks_dir / "java_single_method.json",
-            target_patterns,
-            pid,
-            maestro_flow_path,
-            "android"
-        )
-
-    def test_hook_java_multiple_methods(self, pid, hooks_dir, maestro_flow_path):
-        """Test hooking multiple Java methods in a real process."""
-
-        target_patterns = [
-            {
-                "class": "androidx.security.crypto.EncryptedSharedPreferences$Editor",
-                "method": "putString",
-            },
-            {
-                "class": "androidx.security.crypto.EncryptedSharedPreferences$Editor",
-                "method": "putStringSet",
-            },
-        ]
-
-        run_hook_test(
-            hooks_dir / "java_multiple_methods.json",
+            hooks_dir / "java_single_method_overload.json",
             target_patterns,
             pid,
             maestro_flow_path,
