022a814c296e7b3af01394ff182814b38687b9dd
bernhste <s.bernhardsgruetter@proton.me>
2026-02-04 07:28:20 +0100

    refactoring integration testing to make it more generic

--- tests/integration/test_valid_lifecycle_android.py
+++ tests/integration/test_valid_lifecycle_android.py
@@ -1,26 +1,45 @@
 """Tests for good case lifecycle on Android."""
-from helpers.android_test_helpers import AndroidTestHelper
-
+from conftest import run_hook_test
 
 class TestHookJavaMethod:
     """Tests for handling errors on the target related to Java methods."""
 
     def test_hook_java_single_method(self, pid, hooks_dir, maestro_flow_path):
         """Test hooking a single Java method in a real process."""
-        AndroidTestHelper.run_hook_test(
+
+        target_patterns = [
+            {
+                "class": "android.app.SharedPreferencesImpl$EditorImpl",
+                "method": "putString",
+            }
+        ]
+
+        run_hook_test(
             hooks_dir / "java_single_method.json",
-            "android.app.SharedPreferencesImpl$EditorImpl",
-            ["putString"],
+            target_patterns,
             pid,
-            maestro_flow_path
+            maestro_flow_path,
+            "android"
         )
 
     def test_hook_java_multiple_methods(self, pid, hooks_dir, maestro_flow_path):
         """Test hooking multiple Java methods in a real process."""
-        AndroidTestHelper.run_hook_test(
+
+        target_patterns = [
+            {
+                "class": "androidx.security.crypto.EncryptedSharedPreferences$Editor",
+                "method": "putString",
+            },
+            {
+                "class": "androidx.security.crypto.EncryptedSharedPreferences$Editor",
+                "method": "putStringSet",
+            },
+        ]
+
+        run_hook_test(
             hooks_dir / "java_multiple_methods.json",
-            "androidx.security.crypto.EncryptedSharedPreferences$Editor",
-            ["putString", "putStringSet"],
+            target_patterns,
             pid,
-            maestro_flow_path
+            maestro_flow_path,
+            "android"
         )
