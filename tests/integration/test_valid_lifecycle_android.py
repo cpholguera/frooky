726b4ee1d86fc8f02c2c442dbd3b85c5c669e100
bernhste <s.bernhardsgruetter@proton.me>
2026-02-05 07:27:21 +0100

    refactored tests to use the given-when-then pattern.

--- tests/integration/test_valid_lifecycle_android.py
+++ tests/integration/test_valid_lifecycle_android.py
@@ -1,11 +1,27 @@
 """Tests for good case lifecycle on Android."""
-from conftest import run_hook_test
+from conftest import run_frooky, contains_subset_of
+
 
 class TestHookJavaMethod:
     """Tests for handling errors on the target related to Java methods."""
 
-    def test_hook_java_single_method(self, pid, hooks_dir, maestro_flow_path):
+    def test_hook_java_single_method(self, pid, output_file_path, org_owasp_mastestapp_start):
         """Test hooking a single Java method in a real process."""
+
+        hooks = {
+            "category": "STORAGE",
+            "hooks": [
+                {
+                        "class": "android.app.SharedPreferencesImpl$EditorImpl",
+                        "methods": [
+                            "putString"
+                        ]
+                }
+            ]
+        }
+
+        run_frooky("android", hooks, pid, output_file_path,
+                   org_owasp_mastestapp_start)
 
         expected_patterns = [
             {
@@ -14,16 +30,28 @@ class TestHookJavaMethod:
             }
         ]
 
-        run_hook_test(
-            hooks_dir / "java_single_method.json",
-            expected_patterns,
-            pid,
-            maestro_flow_path,
-            "android"
-        )
+        assert output_file_path.exists(), "output.json was not created"
+        assert contains_subset_of(
+            expected_patterns, output_file_path), "output.json did not contain the expected pattern as a subset."
 
-    def test_hook_java_multiple_methods(self, pid, hooks_dir, maestro_flow_path):
+    def test_hook_java_multiple_methods(self, pid, output_file_path, org_owasp_mastestapp_start):
         """Test hooking multiple Java methods in a real process."""
+
+        hooks = {
+            "category": "STORAGE",
+            "hooks": [
+                {
+                        "class": "androidx.security.crypto.EncryptedSharedPreferences$Editor",
+                        "methods": [
+                            "putString",
+                            "putStringSet"
+                        ]
+                }
+            ]
+        }
+
+        run_frooky("android", hooks, pid, output_file_path,
+                   org_owasp_mastestapp_start)
 
         expected_patterns = [
             {
@@ -36,46 +64,61 @@ class TestHookJavaMethod:
             },
         ]
 
-        run_hook_test(
-            hooks_dir / "java_multiple_methods.json",
-            expected_patterns,
-            pid,
-            maestro_flow_path,
-            "android"
-        )
+        assert output_file_path.exists(), "output.json was not created"
+        assert contains_subset_of(
+            expected_patterns, output_file_path), "output.json did not contain the expected pattern as a subset."
 
-
-    def test_hook_java_single_method_overload(self, pid, hooks_dir, maestro_flow_path):
+    def test_hook_java_single_method_overload(self, pid, output_file_path, org_owasp_mastestapp_start):
         """Test hooking single Java methods and one overload."""
+
+        hooks = {
+            "category": "STORAGE",
+            "hooks": [
+                {
+                        "class": "androidx.security.crypto.EncryptedSharedPreferences",
+                        "method": "create",
+                        "overloads": [
+                            {
+                                "args": [
+                                    "android.content.Context",
+                                    "java.lang.String",
+                                    "androidx.security.crypto.MasterKey",
+                                    "androidx.security.crypto.EncryptedSharedPreferences$PrefKeyEncryptionScheme",
+                                    "androidx.security.crypto.EncryptedSharedPreferences$PrefValueEncryptionScheme"
+                                ]
+                            }
+                        ]
+                }
+            ]
+        }
+
+        run_frooky("android", hooks, pid, output_file_path,
+                   org_owasp_mastestapp_start)
 
         expected_patterns = [
             {
                 "class": "androidx.security.crypto.EncryptedSharedPreferences",
                 "method": "create",
                 "inputParameters": [
-                        {
-                            "declaredType": "android.content.Context"
-                        },
-                        {
-                            "declaredType": "java.lang.String"
-                        },
-                        {
-                            "declaredType": "androidx.security.crypto.MasterKey"
-                        },
-                        {
-                            "declaredType": "androidx.security.crypto.EncryptedSharedPreferences$PrefKeyEncryptionScheme"
-                        },
-                        {
-                            "declaredType": "androidx.security.crypto.EncryptedSharedPreferences$PrefValueEncryptionScheme"
-                        }
-                    ],
+                    {
+                        "declaredType": "android.content.Context"
+                    },
+                    {
+                        "declaredType": "java.lang.String"
+                    },
+                    {
+                        "declaredType": "androidx.security.crypto.MasterKey"
+                    },
+                    {
+                        "declaredType": "androidx.security.crypto.EncryptedSharedPreferences$PrefKeyEncryptionScheme"
+                    },
+                    {
+                        "declaredType": "androidx.security.crypto.EncryptedSharedPreferences$PrefValueEncryptionScheme"
+                    }
+                ],
             }
         ]
 
-        run_hook_test(
-            hooks_dir / "java_single_method_overload.json",
-            expected_patterns,
-            pid,
-            maestro_flow_path,
-            "android"
-        )
+        assert output_file_path.exists(), "output.json was not created"
+        assert contains_subset_of(
+            expected_patterns, output_file_path), "output.json did not contain the expected pattern as a subset."
