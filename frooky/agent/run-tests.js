5afd1f0564ef1b58387ec6f0208003c88ad63600
bernhste <s.bernhardsgruetter@proton.me>
2026-03-31 16:04:25 +0200

    updated ios start script

--- frooky/agent/run-tests.js
+++ frooky/agent/run-tests.js
@@ -31,18 +31,10 @@ validateInput();
 
 async function runTests() {
 
-    let session;
-    let device;
+    let device = usbOption ? await frida.getUsbDevice() : await frida.getLocalDevice();
 
-    if (usbOption){
-        device = await frida.getUsbDevice();
-        const pid = await device.spawn([appIdentifier]);
-        session = await device.attach(pid);
-    } else {
-        // app is already running locally (e.g. GitHub Workflow / iOS Simulator)
-        device = await frida.getLocalDevice()
-        session = await device.attach(appIdentifier);
-    }
+    const pid = await device.spawn([appIdentifier]);
+    const session = await device.attach(appIdentifier);
 
     const distDir = path.join(__dirname, 'dist');
     const agentPath = path.join(distDir, `agent-test-${platformOption}.js`)
