8ff87103bdadedc3a54baefe40df83cecc70677f
bernhste <s.bernhardsgruetter@proton.me>
2026-04-05 08:32:12 +0200

    fix: updated type imports for the agent testing framework

---
+++ frooky/agent/types/agent-test-framework.d.ts
@@ -0,0 +1,15 @@
+// very simple testing framework runnable in a frida environment
+
+declare global {
+    function test(name: string, fn: () => void | Promise<void>): void;
+
+    function expect(actual: any): {
+        toBe(expected: any): void;
+        toEqual(expected: any): void;
+        toBeTruthy(): void;
+        toBeFalsy(): void;
+        toThrow(): void;
+    };
+}
+
+export {};
