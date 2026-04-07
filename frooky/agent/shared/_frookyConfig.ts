6b0d8a5ea2bb5c66a195530312fdb89d600838ff
bernhste <s.bernhardsgruetter@proton.me>
2026-04-07 11:12:42 +0200

    added main application and some stub functions

---
+++ frooky/agent/shared/_frookyConfig.ts
@@ -0,0 +1,7 @@
+import type { FrookyConfig } from "frooky";
+
+// !!!! Don't change this file !!!!!
+// This file will be overwritten when the frida agent is compiled
+// !!!! Don't change this file !!!!!
+const frookyConfigJson = {}
+export const frookyConfig = {} as FrookyConfig
