ad20c55e1206cc40c4081b151e694d570d9f331e
bernhste <s.bernhardsgruetter@proton.me>
2026-04-08 16:56:50 +0200

    updated notes

--- frooky/agent/android/hook/JavaHookRunner.ts
+++ frooky/agent/android/hook/JavaHookRunner.ts
@@ -5,25 +5,30 @@ import type { HookOperation, HookRunner, OperationBuilderResult } from "../../sh
 
 export interface JavaHookOperation extends HookOperation {
     class: string;
-    method: any;
+    method: any;        // Todo needs to be refactored when legacy code is refactored
     overloadIndex: number;
     args: string[];
 }
 
+
+
 export class JavaHookRunner implements HookRunner {
 
-  operationsBuilder(hooks: JavaHook[]): OperationBuilderResult {
-    var operationsArray: JavaHookOperation = [];
+  operationsBuilder(hooks: JavaHook[]): OperationBuilderResult[] {
+    var OperationBuilderResultArray: OperationBuilderResult[] = [];
     hooks.forEach((h: JavaHook) => {
+      // !!!!!!!!!!!!!!!!!!!!!!!!!!!! 
       // TODO: JUMP to legacy code
       // Needs to be refactored later
-      operationsArray.push(buildHookOperations(h))
+      // Also, the naming is pretty confusing, should be refactored later
+      // We should use the validators for the result set, just like with config and hook validations
+      // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      OperationBuilderResultArray.push(buildHookOperations(h))
     })
 
+    frooky.log.info(`Hook operations for the following hooks built: ${JSON.stringify(OperationBuilderResultArray)}`)
 
-    console.log(JSON.stringify(operationsArray))
-
-    return operationsArray;
+    return OperationBuilderResultArray;
   }
 
   executeHooking(operations: HookOperation[]): void {
