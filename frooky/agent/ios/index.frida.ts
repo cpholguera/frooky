5b243faac467fba8313be7550d886792a38fb25d
bernhste <s.bernhardsgruetter@proton.me>
2026-04-08 20:07:44 +0200

    added ios warning

--- frooky/agent/ios/index.frida.ts
+++ frooky/agent/ios/index.frida.ts
@@ -1,22 +1,26 @@
-// This file is used when the agent is run by frida. Hooks embedded into the agent at build time.
-// !!!! Don't change this file, the build script will insert the actual frooky config here
-import ObjC from "frida-objc-bridge";
-import type { FrookyConfig } from "frooky";
-import { FrookyApp } from "../FrookyApp";
+// // This file is used when the agent is run by frida. Hooks embedded into the agent at build time.
+// // !!!! Don't change this file, the build script will insert the actual frooky config here
+// import ObjC from "frida-objc-bridge";
+// import type { FrookyConfig } from "frooky";
+// import { FrookyApp } from "../FrookyApp";
 
-var frookyConfigs: FrookyConfig[];
+// var frookyConfigs: FrookyConfig[];
 
-if (ObjC.available) {
-//%%% REPLACE START
-    frookyConfigs = { } as FrookyConfig[];
-//%%% REPLACE STOP
+// if (ObjC.available) {
+// //%%% REPLACE START
+//     frookyConfigs = { } as FrookyConfig[];
+// //%%% REPLACE STOP
 
-    globalThis.frooky = new FrookyApp("iOS", 3, "device");
-    frookyConfigs.forEach(frookyConfig => {
-        frooky.loadFrookyConfig(frookyConfig);
-    });
-    frooky.run();
+//     globalThis.frooky = new FrookyApp("iOS", 3, "device");
+//     frookyConfigs.forEach(frookyConfig => {
+//         frooky.loadFrookyConfig(frookyConfig);
+//     });
+//     frooky.prepareHookOperation();
+//     frooky.executeHookOperations();
 
-} else {
-  console.error("[!] The agent is not run on an iOS device. Make sure to run this version of the frooky agent on iOS.")
-}
+// } else {
+//   console.error("[!] The agent is not run on an iOS device. Make sure to run this version of the frooky agent on iOS.")
+// }
+
+
+throw Error("This is the PoC branch for the agent refactoring. At the moment, iOS is not yet implemented.")
--- frooky/agent/ios/index.frooky.ts
+++ frooky/agent/ios/index.frooky.ts
@@ -1,17 +1,23 @@
-// This file is used when the agent is run by frooky. Hooks are dynamically loaded using rpc at runtime.
+// // This file is used when the agent is run by frooky. Hooks are dynamically loaded using rpc at runtime.
 
-import ObjC from "frida-objc-bridge";
-import type { FrookyConfig } from "frooky";
-import { FrookyApp } from "../FrookyApp";
+// import ObjC from "frida-objc-bridge";
+// import type { FrookyConfig } from "frooky";
+// import { FrookyApp } from "../FrookyApp";
 
-if (ObjC.available) {
-  rpc.exports = {
-    runFrookyAgent(frookyConfig: FrookyConfig) {
-      globalThis.frooky = new FrookyApp("iOS");
-      frooky.loadFrookyConfig(frookyConfig);
-      frooky.run(); 
-    }
-  };
-} else {
-  console.error("[!] The agent is not run on an iOS device. Make sure to run this version of the frooky agent on iOS.")
-}
+// if (ObjC.available) {
+//   rpc.exports = {
+//     runFrookyAgent(frookyConfig: FrookyConfig) {
+//       globalThis.frooky = new FrookyApp("iOS");
+//       frooky.loadFrookyConfig(frookyConfig);
+
+//       frooky.prepareHookOperation();
+//       frooky.executeHookOperations();
+
+//     }
+//   };
+// } else {
+//   console.error("[!] The agent is not run on an iOS device. Make sure to run this version of the frooky agent on iOS.")
+// }
+
+
+throw Error("This is the PoC branch for the agent refactoring. At the moment, iOS is not yet implemented.")
