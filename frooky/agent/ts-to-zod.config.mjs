b0e76698e5130d11042d9a377010bea7989b0dc7
bernhste <s.bernhardsgruetter@proton.me>
2026-04-08 23:18:46 +0200

    reorganized types

--- frooky/agent/ts-to-zod.config.mjs
+++ frooky/agent/ts-to-zod.config.mjs
@@ -4,11 +4,11 @@
  * @type {import("ts-to-zod").TsToZodConfig}
  */
 export default [
-    {"name":"parameter","input":"types/parameter.d.ts","output":"types/parameter.zod.ts"},
-    {"name":"returnType","input":"types/returnType.d.ts","output":"types/returnType.zod.ts"},
+    {"name":"parameter","input":"types/parameter.ts","output":"types/parameter.zod.ts"},
+    {"name":"returnType","input":"types/returnType.ts","output":"types/returnType.zod.ts"},
 
-    {"name":"baseHook",  "input":"types/hook/baseHook.d.ts","output":"types/hook/baseHook.zod.ts"},
-    {"name":"javaHook",  "input":"types/hook/javaHook.d.ts","output":"types/hook/javaHook.zod.ts"},
-    {"name":"objcHook",  "input":"types/hook/objcHook.d.ts","output":"types/hook/objcHook.zod.ts"},
-    {"name":"nativeHook","input":"types/hook/nativeHook.d.ts","output":"types/hook/nativeHook.zod.ts"}
+    {"name":"baseHook",  "input":"types/hook/baseHook.ts","output":"types/hook/baseHook.zod.ts"},
+    {"name":"javaHook",  "input":"types/hook/javaHook.ts","output":"types/hook/javaHook.zod.ts"},
+    {"name":"objcHook",  "input":"types/hook/objcHook.ts","output":"types/hook/objcHook.zod.ts"},
+    {"name":"nativeHook","input":"types/hook/nativeHook.ts","output":"types/hook/nativeHook.zod.ts"}
 ]
