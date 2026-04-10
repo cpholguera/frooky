4065a642a511dd1425d6c6076993840d2687008d
bernhste <s.bernhardsgruetter@proton.me>
2026-04-10 15:32:42 +0200

    linting

--- frooky/agent/shared/validator/hookValidator.ts
+++ frooky/agent/shared/validator/hookValidator.ts
@@ -1,76 +1,68 @@
 import type { Hook, HookMetadata, JavaHook, NativeHook, ObjcHook, Platform } from "frooky";
 import { isJavaHook, isNativeHook, isObjcHook } from "frooky";
-import { normalizeJavaHook, type JavaHookInput } from "../inputParsing/javaHookInput";
-import { normalizeObjcHook, type ObjcHookInput } from "../inputParsing/objcHookInput";
-
 import z from "zod";
+import { type JavaHookInput, normalizeJavaHook } from "../inputParsing/javaHookInput";
+import { normalizeNativeHook } from "../inputParsing/nativeHookInput";
+import { normalizeObjcHook, type ObjcHookInput } from "../inputParsing/objcHookInput";
 import { javaHookInputSchema } from "../inputParsing/zodSchemas/javaHook.input.zod";
 import { nativeHookInputSchema } from "../inputParsing/zodSchemas/nativeHook.input.zod";
 import { objcHookInputSchema } from "../inputParsing/zodSchemas/objcHook.input.zod";
 import { prettyPrintHook } from "../utils";
-import { normalizeNativeHook } from "../inputParsing/nativeHookInput";
-
-
 
 export interface HookValidatorResult {
-    validHooks: Hook[];
-    invalidHooks: Hook[];
-    totalHooks: number;
-    totalErrors: number;
+  validHooks: Hook[];
+  invalidHooks: Hook[];
+  totalHooks: number;
+  totalErrors: number;
 }
 
+export function validateHooks(hooks: Hook[], platform: Platform, metadata?: HookMetadata): HookValidatorResult {
+  const result: HookValidatorResult = {
+    validHooks: [],
+    invalidHooks: [],
+    totalHooks: 0,
+    totalErrors: 0,
+  };
 
-export function validateHooks(hooks: Hook[], platform: Platform, metadata?: HookMetadata,): HookValidatorResult {
+  hooks.forEach((hook) => {
+    result.totalHooks += 1;
 
-    const result: HookValidatorResult = {
-        validHooks: [],
-        invalidHooks: [],
-        totalHooks: 0,
-        totalErrors: 0
-    };
+    // Merge config metadata into hook metadata
+    if (metadata) {
+      hook.metadata = { ...metadata, ...hook.metadata };
+    }
 
+    try {
+      if (platform !== "Android") {
+        throw new Error(`Skipped the following hook, as it is not compatible with ${platform}: \n${prettyPrintHook(hook)}`);
+      }
+      if (isJavaHook(hook)) {
+        javaHookInputSchema.parse(hook as JavaHookInput);
+        const javaHook = normalizeJavaHook(hook) as JavaHook;
+        result.validHooks.push(javaHook);
+      } else if (isObjcHook(hook)) {
+        objcHookInputSchema.parse(hook as ObjcHookInput);
+        const objcHook = normalizeObjcHook(hook) as ObjcHook;
+        result.validHooks.push(objcHook);
+      } else if (isNativeHook(hook)) {
+        nativeHookInputSchema.parse(hook as NativeHook);
+        const nativeHook = normalizeNativeHook(hook) as NativeHook;
+        result.validHooks.push(nativeHook);
+      } else {
+        throw new Error("Hook type is unknown. Make sure that it is either a Java, Objective-C or native hook.");
+      }
+    } catch (error) {
+      result.totalErrors += 1;
+      result.invalidHooks.push(hook);
+      if (error instanceof z.ZodError) {
+        frooky.log.error(`Hook is not according to schema: ${JSON.stringify(z.treeifyError(error), null, 2)}`);
+      } else {
+        frooky.log.error(error as string);
+      }
+    }
+  });
 
-    hooks.forEach(hook => {
-        result.totalHooks += 1;
+  frooky.log.info("All hooks validated.");
 
-        // Merge config metadata into hook metadata
-        if (metadata) {
-            hook.metadata = { ...metadata, ...hook.metadata };
-        }
-
-        try {
-            if (platform !== "Android") {
-                throw new Error(`Skipped the following hook, as it is not compatible with ${platform}: \n${prettyPrintHook(hook)}`);
-            }
-            if (isJavaHook(hook)) {
-                javaHookInputSchema.parse(hook as JavaHookInput);
-                const javaHook = normalizeJavaHook(hook) as JavaHook;
-                result.validHooks.push(javaHook);
-
-            } else if (isObjcHook(hook)) {
-                objcHookInputSchema.parse(hook as ObjcHookInput);
-                const objcHook = normalizeObjcHook(hook) as ObjcHook;
-                result.validHooks.push(objcHook);
-
-            } else if (isNativeHook(hook)) {
-                nativeHookInputSchema.parse(hook as NativeHook);
-                const nativeHook = normalizeNativeHook(hook) as NativeHook;
-                result.validHooks.push(nativeHook);
-            } else {
-                throw new Error("Hook type is unknown. Make sure that it is either a Java, Objective-C or native hook.");
-            }
-        } catch (error) {
-            result.totalErrors += 1;
-            result.invalidHooks.push(hook);
-            if (error instanceof z.ZodError) {
-                frooky.log.error(`Hook is not according to schema: ${JSON.stringify(z.treeifyError(error), null, 2)}`);
-            } else {
-                frooky.log.error(error as string);
-            }
-        }
-    });
-
-    frooky.log.info("All hooks validated.")
-
-    return result;
+  return result;
 }
