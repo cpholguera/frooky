ef5be1cc2d48e22f433a2c2bed74a47f369175aa
bernhste <s.bernhardsgruetter@proton.me>
2026-04-15 12:35:06 +0200

    update android install script

--- .github/scripts/android/launch-target-apps.sh
+++ .github/scripts/android/launch-target-apps.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 for apk in target-apps-android/*.apk; do
   TARGET_APP=$(basename "$apk" .target-app.apk)
-  APP_ID="${TARGET_APP//-/_}.mastestapp"
+  APP_ID="${TARGET_APP//-/_}.frooky.target.app"
   echo "=== Launching $APP_ID ==="
   adb shell am start -n "$APP_ID/org.owasp.mastestapp.MainActivity"
 done
