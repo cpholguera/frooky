b6e3b8739a24edbac5e14be52af3b35428ed7ad1
bernhste <s.bernhardsgruetter@proton.me>
2026-04-19 11:41:16 +0200

    added generic iterator decoder

--- frooky/agent/shared/config.ts
+++ frooky/agent/shared/config.ts
@@ -1,3 +1,8 @@
+export const DECODER_MAX_RECURSION = 10;
+
+// maximal amount of elements retrieved on an iterable object
+export const DECODER_MAX_ELEMENTS = 100;
+
 export const DEFAULT_STACK_TRACE_LIMIT = 10;
 
 // specifies the interval between cached events are send back to the host
