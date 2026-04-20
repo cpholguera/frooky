7f11f9610e736667fa2bc22a9e25109d75bdcad2
bernhste <s.bernhardsgruetter@proton.me>
2026-04-20 19:26:52 +0200

    added native app

--- tests/target-apps/android/basic-native/MastgTest.kt
+++ tests/target-apps/android/basic-native/MastgTest.kt
@@ -3,7 +3,18 @@ package org.owasp.mastestapp
 import android.content.Context
 
 class MastgTest(private val context: Context) {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
     fun mastgTest(): String {
-        return "TBD: Implement the target app."
+        val r = DemoResults("basic-native")f
+
+
+        r.add(Status.PASS, "Loaded native library and ran various functions.")
+
+
+        return r.toJson()
     }
 }
