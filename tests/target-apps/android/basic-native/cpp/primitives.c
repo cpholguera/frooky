202a23a2d82b6c5bd203aa4a29e878a1855dd731
bernhste <s.bernhardsgruetter@proton.me>
2026-04-23 16:59:47 +0200

    added pass by ref use cases

--- tests/target-apps/android/basic-native/cpp/primitives.c
+++ tests/target-apps/android/basic-native/cpp/primitives.c
@@ -6,6 +6,7 @@
 #define NOINLINE __attribute__((noinline))
 #define EXPORT   __attribute__((visibility("default")))
 
+/* ---------- pass by value ---------- */
 NOINLINE EXPORT bool               pass_bool(bool v)                 { printf("bool: %d\n", v);       return v; }
 NOINLINE EXPORT char               pass_char(char v)                 { printf("char: %c\n", v);       return v; }
 NOINLINE EXPORT signed char        pass_schar(signed char v)         { printf("schar: %d\n", v);      return v; }
@@ -23,10 +24,29 @@ NOINLINE EXPORT double             pass_double(double v)             { printf("d
 NOINLINE EXPORT long double        pass_ldouble(long double v)       { printf("ldouble: %Lf\n", v);   return v; }
 NOINLINE EXPORT wchar_t            pass_wchar(wchar_t v)             { printf("wchar: %d\n", (int)v); return v; }
 
+/* ---------- pass by reference ---------- */
+NOINLINE EXPORT void pass_bool_ref   (bool *p)               { printf("*bool: %d\n",    *p);       *p = !(*p); }
+NOINLINE EXPORT void pass_char_ref   (char *p)               { printf("*char: %c\n",    *p);       *p += 1; }
+NOINLINE EXPORT void pass_schar_ref  (signed char *p)        { printf("*schar: %d\n",   *p);       *p = -*p; }
+NOINLINE EXPORT void pass_uchar_ref  (unsigned char *p)      { printf("*uchar: %u\n",   *p);       *p += 1; }
+NOINLINE EXPORT void pass_short_ref  (short *p)              { printf("*short: %d\n",   *p);       *p = -*p; }
+NOINLINE EXPORT void pass_ushort_ref (unsigned short *p)     { printf("*ushort: %u\n",  *p);       *p += 1; }
+NOINLINE EXPORT void pass_int_ref    (int *p)                { printf("*int: %d\n",     *p);       *p = -*p; }
+NOINLINE EXPORT void pass_uint_ref   (unsigned int *p)       { printf("*uint: %u\n",    *p);       *p += 1; }
+NOINLINE EXPORT void pass_long_ref   (long *p)               { printf("*long: %ld\n",   *p);       *p = -*p; }
+NOINLINE EXPORT void pass_ulong_ref  (unsigned long *p)      { printf("*ulong: %lu\n",  *p);       *p += 1; }
+NOINLINE EXPORT void pass_llong_ref  (long long *p)          { printf("*llong: %lld\n", *p);       *p = -*p; }
+NOINLINE EXPORT void pass_ullong_ref (unsigned long long *p) { printf("*ullong: %llu\n",*p);       *p += 1; }
+NOINLINE EXPORT void pass_float_ref  (float *p)              { printf("*float: %f\n",   *p);       *p = -*p; }
+NOINLINE EXPORT void pass_double_ref (double *p)             { printf("*double: %f\n",  *p);       *p = -*p; }
+NOINLINE EXPORT void pass_ldouble_ref(long double *p)        { printf("*ldouble: %Lf\n",*p);       *p = -*p; }
+NOINLINE EXPORT void pass_wchar_ref  (wchar_t *p)            { printf("*wchar: %d\n",  (int)*p);   *p += 1; }
+
 JNIEXPORT jstring JNICALL
 Java_org_owasp_mastestapp_MastgTest_passPrimitivesJNI(JNIEnv *env, jobject thiz) {
-    (void)thiz; /* silence unused-parameter warning */
+    (void)thiz;
 
+    /* by value */
     pass_bool(true);
     pass_char('A');
     pass_schar(-5);
@@ -44,5 +64,40 @@ Java_org_owasp_mastestapp_MastgTest_passPrimitivesJNI(JNIEnv *env, jobject thiz)
     pass_ldouble(1.4142135623730951L);
     pass_wchar(L'Z');
 
-    return (*env)->NewStringUTF(env, "Called a functions with primitives.");
+    /* by reference */
+    bool               b   = true;
+    char               c   = 'A';
+    signed char        sc  = -5;
+    unsigned char      uc  = 250;
+    short              s   = -1234;
+    unsigned short     us  = 1234;
+    int                i   = -42;
+    unsigned int       ui  = 42u;
+    long               l   = -100000L;
+    unsigned long      ul  = 100000UL;
+    long long          ll  = -1234567890123LL;
+    unsigned long long ull = 1234567890123ULL;
+    float              f   = 3.14f;
+    double             d   = 2.718281828;
+    long double        ld  = 1.4142135623730951L;
+    wchar_t            wc  = L'Z';
+
+    pass_bool_ref(&b);
+    pass_char_ref(&c);
+    pass_schar_ref(&sc);
+    pass_uchar_ref(&uc);
+    pass_short_ref(&s);
+    pass_ushort_ref(&us);
+    pass_int_ref(&i);
+    pass_uint_ref(&ui);
+    pass_long_ref(&l);
+    pass_ulong_ref(&ul);
+    pass_llong_ref(&ll);
+    pass_ullong_ref(&ull);
+    pass_float_ref(&f);
+    pass_double_ref(&d);
+    pass_ldouble_ref(&ld);
+    pass_wchar_ref(&wc);
+
+    return (*env)->NewStringUTF(env, "Called functions with primitives (by value and by reference).");
 }
