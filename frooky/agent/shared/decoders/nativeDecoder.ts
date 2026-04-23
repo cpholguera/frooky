f3ab3f2fd1f6757d52d4f44dc1b3a096dd591e53
bernhste <s.bernhardsgruetter@proton.me>
2026-04-23 08:54:46 +0200

    updated native decoder

--- frooky/agent/shared/decoders/nativeDecoder.ts
+++ frooky/agent/shared/decoders/nativeDecoder.ts
@@ -3,30 +3,39 @@ import type { Param } from "../../shared/hook/parameter";
 import { nativeDecoderRegistry } from "./nativeDecoderRegistry";
 
 const typeDecoderMap: Record<string, Decoder<NativePointer>> = {
-  "int8_t":         { decode: (ptr, param) => ({ type: param.type, value: ptr.readS8() }) },
-  "uint8_t":        { decode: (ptr, param) => ({ type: param.type, value: ptr.readU8() }) },
-  "int16_t":        { decode: (ptr, param) => ({ type: param.type, value: ptr.readS16() }) },
-  "uint16_t":       { decode: (ptr, param) => ({ type: param.type, value: ptr.readU16() }) },
-  "short":          { decode: (ptr, param) => ({ type: param.type, value: ptr.readShort() }) },
-  "unsigned short": { decode: (ptr, param) => ({ type: param.type, value: ptr.readUShort() }) },
-  "int32_t":        { decode: (ptr, param) => ({ type: param.type, value: ptr.readS32() }) },
-  "uint32_t":       { decode: (ptr, param) => ({ type: param.type, value: ptr.readU32() }) },
-  "int":            { decode: (ptr, param) => ({ type: param.type, value: ptr.readInt() }) },
-  "unsigned int":   { decode: (ptr, param) => ({ type: param.type, value: ptr.readUInt() }) },
-  "float":          { decode: (ptr, param) => ({ type: param.type, value: ptr.readFloat() }) },
-  "double":         { decode: (ptr, param) => ({ type: param.type, value: ptr.readDouble() }) },
-  "void *":         { decode: (ptr, param) => ({ type: param.type, value: ptr.readPointer() }) },
-  "int64_t":        { decode: (ptr, param) => ({ type: param.type, value: ptr.readS64() }) },
-  "uint64_t":       { decode: (ptr, param) => ({ type: param.type, value: ptr.readU64() }) },
-  "long":           { decode: (ptr, param) => ({ type: param.type, value: ptr.readLong() }) },
-  "unsigned long":  { decode: (ptr, param) => ({ type: param.type, value: ptr.readULong() }) },
-  "char *":         { decode: (ptr, param) => ({ type: param.type, value: ptr.readCString() }) },
-  "char * (utf8)":  { decode: (ptr, param) => ({ type: param.type, value: ptr.readUtf8String() }) },
-  "char16_t *":     { decode: (ptr, param) => ({ type: param.type, value: ptr.readUtf16String() }) },
-  "char * (ansi)":  { decode: (ptr, param) => ({ type: param.type, value: ptr.readAnsiString() }) },
+  "int8_t":         { decode: (input, param) => ({ type: param.type, value: input.readS8() }) },
+  "uint8_t":        { decode: (input, param) => ({ type: param.type, value: input.readU8() }) },
+  "int16_t":        { decode: (input, param) => ({ type: param.type, value: input.readS16() }) },
+  "uint16_t":       { decode: (input, param) => ({ type: param.type, value: input.readU16() }) },
+  "short":          { decode: (input, param) => ({ type: param.type, value: input.readShort() }) },
+  "unsigned short": { decode: (input, param) => ({ type: param.type, value: input.readUShort() }) },
+  "int32_t":        { decode: (input, param) => ({ type: param.type, value: input.readS32() }) },
+  "uint32_t":       { decode: (input, param) => ({ type: param.type, value: input.readU32() }) },
+  "int":            { decode: (input, param) => ({ type: param.type, value: input.readInt() }) },
+  "unsigned int":   { decode: (input, param) => ({ type: param.type, value: input.readUInt() }) },
+  "float":          { decode: (input, param) => ({ type: param.type, value: input.readFloat() }) },
+  "double":         { decode: (input, param) => ({ type: param.type, value: input.readDouble() }) },
+  "void *":         { decode: (input, param) => ({ type: param.type, value: input.readPointer() }) },
+  "int64_t":        { decode: (input, param) => ({ type: param.type, value: input.readS64() }) },
+  "uint64_t":       { decode: (input, param) => ({ type: param.type, value: input.readU64() }) },
+  "long":           { decode: (input, param) => ({ type: param.type, value: input.readLong() }) },
+  "unsigned long":  { decode: (input, param) => ({ type: param.type, value: input.readULong() }) },
+  "char *":         { decode: (input, param) => ({ type: param.type, value: input.readCString() }) },
+  "char * (utf8)":  { decode: (input, param) => ({ type: param.type, value: input.readUtf8String() }) },
+  "char16_t *":     { decode: (input, param) => ({ type: param.type, value: input.readUtf16String() }) },
+  "char * (ansi)":  { decode: (input, param) => ({ type: param.type, value: input.readAnsiString() }) },
 };
 
 
+const FallbackNativeDecoder: Decoder<NativePointer> = {
+  decode: (input: NativePointer, param: Param): DecodedValue => {
+    return {
+      type: param.implementationType ?? param.type,
+      name: param.name,
+      value: "<NO-DECODER-IMPLEMENTED>"
+    };
+  },
+};
 /**
  * Resolve the concrete Decoder for `input` under `param`.
  * Called only on the first invocation for a given Param; the result is cached
@@ -35,8 +44,8 @@ const typeDecoderMap: Record<string, Decoder<NativePointer>> = {
 function resolveNativeDecoder(param: Param): Decoder<NativePointer> {
   const decoder = typeDecoderMap[param.type];
   if (!decoder) {
-    throw new Error(`No decoder found for type: ${param.type}`);
-  }
+    return FallbackNativeDecoder;
+  } else 
   return decoder;
 }
 
@@ -55,6 +64,7 @@ export const NativeDecoder: Decoder<NativePointer> = {
     // Resolve the decoder from the frooky parameter declaration and cache it
       const decoder = resolveNativeDecoder(param);
       param.decoder = decoder;
+      console.log(decoder)
       return decoder.decode(input, param, quickDecode);
   },
 };
