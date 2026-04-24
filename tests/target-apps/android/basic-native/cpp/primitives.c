ad4751da476a7f8265e889d815dbc552946cf3d1
bernhste <s.bernhardsgruetter@proton.me>
2026-04-24 21:10:09 +0200

    updated native method to pass min/max values

--- tests/target-apps/android/basic-native/cpp/primitives.c
+++ tests/target-apps/android/basic-native/cpp/primitives.c
@@ -1,68 +1,64 @@
 #include <jni.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <wchar.h>
 
 #define NOINLINE __attribute__((noinline))
 #define EXPORT   __attribute__((visibility("default")))
 
 /* ---------- pass by value ---------- */
-NOINLINE EXPORT bool               pass_bool(bool v)                 { printf("bool: %d\n", v);       return v; }
-NOINLINE EXPORT char               pass_char(char v)                 { printf("char: %c\n", v);       return v; }
-NOINLINE EXPORT signed char        pass_schar(signed char v)         { printf("schar: %d\n", v);      return v; }
-NOINLINE EXPORT unsigned char      pass_uchar(unsigned char v)       { printf("uchar: %u\n", v);      return v; }
-NOINLINE EXPORT short              pass_short(short v)               { printf("short: %d\n", v);      return v; }
-NOINLINE EXPORT unsigned short     pass_ushort(unsigned short v)     { printf("ushort: %u\n", v);     return v; }
-NOINLINE EXPORT int                pass_int(int v)                   { printf("int: %d\n", v);        return v; }
-NOINLINE EXPORT unsigned int       pass_uint(unsigned int v)         { printf("uint: %u\n", v);       return v; }
-NOINLINE EXPORT long               pass_long(long v)                 { printf("long: %ld\n", v);      return v; }
-NOINLINE EXPORT unsigned long      pass_ulong(unsigned long v)       { printf("ulong: %lu\n", v);     return v; }
-NOINLINE EXPORT long long          pass_llong(long long v)           { printf("llong: %lld\n", v);    return v; }
-NOINLINE EXPORT unsigned long long pass_ullong(unsigned long long v) { printf("ullong: %llu\n", v);   return v; }
-NOINLINE EXPORT float              pass_float(float v)               { printf("float: %f\n", v);      return v; }
-NOINLINE EXPORT double             pass_double(double v)             { printf("double: %f\n", v);     return v; }
-NOINLINE EXPORT long double        pass_ldouble(long double v)       { printf("ldouble: %Lf\n", v);   return v; }
-NOINLINE EXPORT wchar_t            pass_wchar(wchar_t v)             { printf("wchar: %d\n", (int)v); return v; }
+NOINLINE EXPORT bool               pass_bool   (bool minValue,               bool maxValue)               { return minValue; }
+NOINLINE EXPORT char               pass_char   (char minValue,               char maxValue)               { return minValue; }
+NOINLINE EXPORT signed char        pass_schar  (signed char minValue,        signed char maxValue)        { return minValue; }
+NOINLINE EXPORT unsigned char      pass_uchar  (unsigned char minValue,      unsigned char maxValue)      { return minValue; }
+NOINLINE EXPORT short              pass_short  (short minValue,              short maxValue)              { return minValue; }
+NOINLINE EXPORT unsigned short     pass_ushort (unsigned short minValue,     unsigned short maxValue)     { return minValue; }
+NOINLINE EXPORT int                pass_int    (int minValue,                int maxValue)                { return minValue; }
+NOINLINE EXPORT unsigned int       pass_uint   (unsigned int minValue,       unsigned int maxValue)       { return minValue; }
+NOINLINE EXPORT long               pass_long   (long minValue,               long maxValue)               { return minValue; }
+NOINLINE EXPORT unsigned long      pass_ulong  (unsigned long minValue,      unsigned long maxValue)      { return minValue; }
+NOINLINE EXPORT long long          pass_llong  (long long minValue,          long long maxValue)          { return minValue; }
+NOINLINE EXPORT unsigned long long pass_ullong (unsigned long long minValue, unsigned long long maxValue) { return minValue; }
+NOINLINE EXPORT float              pass_float  (float minValue,              float maxValue)              { return minValue; }
+NOINLINE EXPORT double             pass_double (double minValue,             double maxValue)             { return minValue; }
+NOINLINE EXPORT long double        pass_ldouble(long double minValue,        long double maxValue)        { return minValue; }
 
 /* ---------- pass by reference ---------- */
-NOINLINE EXPORT void pass_bool_ref   (bool *p)               { printf("*bool: %d\n",    *p);       *p = !(*p); }
-NOINLINE EXPORT void pass_char_ref   (char *p)               { printf("*char: %c\n",    *p);       *p += 1; }
-NOINLINE EXPORT void pass_schar_ref  (signed char *p)        { printf("*schar: %d\n",   *p);       *p = -*p; }
-NOINLINE EXPORT void pass_uchar_ref  (unsigned char *p)      { printf("*uchar: %u\n",   *p);       *p += 1; }
-NOINLINE EXPORT void pass_short_ref  (short *p)              { printf("*short: %d\n",   *p);       *p = -*p; }
-NOINLINE EXPORT void pass_ushort_ref (unsigned short *p)     { printf("*ushort: %u\n",  *p);       *p += 1; }
-NOINLINE EXPORT void pass_int_ref    (int *p)                { printf("*int: %d\n",     *p);       *p = -*p; }
-NOINLINE EXPORT void pass_uint_ref   (unsigned int *p)       { printf("*uint: %u\n",    *p);       *p += 1; }
-NOINLINE EXPORT void pass_long_ref   (long *p)               { printf("*long: %ld\n",   *p);       *p = -*p; }
-NOINLINE EXPORT void pass_ulong_ref  (unsigned long *p)      { printf("*ulong: %lu\n",  *p);       *p += 1; }
-NOINLINE EXPORT void pass_llong_ref  (long long *p)          { printf("*llong: %lld\n", *p);       *p = -*p; }
-NOINLINE EXPORT void pass_ullong_ref (unsigned long long *p) { printf("*ullong: %llu\n",*p);       *p += 1; }
-NOINLINE EXPORT void pass_float_ref  (float *p)              { printf("*float: %f\n",   *p);       *p = -*p; }
-NOINLINE EXPORT void pass_double_ref (double *p)             { printf("*double: %f\n",  *p);       *p = -*p; }
-NOINLINE EXPORT void pass_ldouble_ref(long double *p)        { printf("*ldouble: %Lf\n",*p);       *p = -*p; }
-NOINLINE EXPORT void pass_wchar_ref  (wchar_t *p)            { printf("*wchar: %d\n",  (int)*p);   *p += 1; }
+NOINLINE EXPORT void pass_bool_ref   (bool *p,               bool minValue,               bool maxValue)               { *p = !(*p); }
+NOINLINE EXPORT void pass_char_ref   (char *p,               char minValue,               char maxValue)               { *p += 1; }
+NOINLINE EXPORT void pass_schar_ref  (signed char *p,        signed char minValue,        signed char maxValue)        { *p = -*p; }
+NOINLINE EXPORT void pass_uchar_ref  (unsigned char *p,      unsigned char minValue,      unsigned char maxValue)      { *p += 1; }
+NOINLINE EXPORT void pass_short_ref  (short *p,              short minValue,              short maxValue)              { *p = -*p; }
+NOINLINE EXPORT void pass_ushort_ref (unsigned short *p,     unsigned short minValue,     unsigned short maxValue)     { *p += 1; }
+NOINLINE EXPORT void pass_int_ref    (int *p,                int minValue,                int maxValue)                { *p = -*p; }
+NOINLINE EXPORT void pass_uint_ref   (unsigned int *p,       unsigned int minValue,       unsigned int maxValue)       { *p += 1; }
+NOINLINE EXPORT void pass_long_ref   (long *p,               long minValue,               long maxValue)               { *p = -*p; }
+NOINLINE EXPORT void pass_ulong_ref  (unsigned long *p,      unsigned long minValue,      unsigned long maxValue)      { *p += 1; }
+NOINLINE EXPORT void pass_llong_ref  (long long *p,          long long minValue,          long long maxValue)          { *p = -*p; }
+NOINLINE EXPORT void pass_ullong_ref (unsigned long long *p, unsigned long long minValue, unsigned long long maxValue) { *p += 1; }
+NOINLINE EXPORT void pass_float_ref  (float *p,              float minValue,              float maxValue)              { *p = -*p; }
+NOINLINE EXPORT void pass_double_ref (double *p,             double minValue,             double maxValue)             { *p = -*p; }
+NOINLINE EXPORT void pass_ldouble_ref(long double *p,        long double minValue,        long double maxValue)        { *p = -*p; }
 
 JNIEXPORT jstring JNICALL
 Java_org_owasp_mastestapp_MastgTest_passPrimitivesJNI(JNIEnv *env, jobject thiz) {
     (void)thiz;
 
     /* by value */
-    pass_bool(true);
-    pass_char('A');
-    pass_schar(-5);
-    pass_uchar(250);
-    pass_short(-1234);
-    pass_ushort(1234);
-    pass_int(-42);
-    pass_uint(42u);
-    pass_long(-100000L);
-    pass_ulong(100000UL);
-    pass_llong(-1234567890123LL);
-    pass_ullong(1234567890123ULL);
-    pass_float(3.14f);
-    pass_double(2.718281828);
-    pass_ldouble(1.4142135623730951L);
-    pass_wchar(L'Z');
+    pass_bool   (false,                   true);
+    pass_char   ('A',                     'Z');
+    pass_schar  (-128,                    127);
+    pass_uchar  (0,                       255);
+    pass_short  (-32768,                  32767);
+    pass_ushort (0,                       65535);
+    pass_int    (-2147483648,             2147483647);
+    pass_uint   (0u,                      4294967295u);
+    pass_long   (-2147483648L,            2147483647L);
+    pass_ulong  (0UL,                     4294967295UL);
+    pass_llong  (-9223372036854775807LL,  9223372036854775807LL);
+    pass_ullong (0ULL,                    18446744073709551615ULL);
+    pass_float  (-3.4028235e38f,          3.4028235e38f);
+    pass_double (-1.7976931348623157e308, 1.7976931348623157e308);
+    pass_ldouble(-1.18973149535723176e4932L, 1.18973149535723176e4932L);
 
     /* by reference */
     bool               b   = true;
@@ -82,22 +78,21 @@ Java_org_owasp_mastestapp_MastgTest_passPrimitivesJNI(JNIEnv *env, jobject thiz)
     long double        ld  = 1.4142135623730951L;
     wchar_t            wc  = L'Z';
 
-    pass_bool_ref(&b);
-    pass_char_ref(&c);
-    pass_schar_ref(&sc);
-    pass_uchar_ref(&uc);
-    pass_short_ref(&s);
-    pass_ushort_ref(&us);
-    pass_int_ref(&i);
-    pass_uint_ref(&ui);
-    pass_long_ref(&l);
-    pass_ulong_ref(&ul);
-    pass_llong_ref(&ll);
-    pass_ullong_ref(&ull);
-    pass_float_ref(&f);
-    pass_double_ref(&d);
-    pass_ldouble_ref(&ld);
-    pass_wchar_ref(&wc);
+    pass_bool_ref   (&b,   false,                    true);
+    pass_char_ref   (&c,   'A',                      'Z');
+    pass_schar_ref  (&sc,  -128,                     127);
+    pass_uchar_ref  (&uc,  0,                        255);
+    pass_short_ref  (&s,   -32768,                   32767);
+    pass_ushort_ref (&us,  0,                        65535);
+    pass_int_ref    (&i,   -2147483648,               2147483647);
+    pass_uint_ref   (&ui,  0u,                        4294967295u);
+    pass_long_ref   (&l,   -2147483648L,              2147483647L);
+    pass_ulong_ref  (&ul,  0UL,                       4294967295UL);
+    pass_llong_ref  (&ll,  -9223372036854775807LL,    9223372036854775807LL);
+    pass_ullong_ref (&ull, 0ULL,                      18446744073709551615ULL);
+    pass_float_ref  (&f,   -3.4028235e38f,            3.4028235e38f);
+    pass_double_ref (&d,   -1.7976931348623157e308,   1.7976931348623157e308);
+    pass_ldouble_ref(&ld,  -1.18973149535723176e4932L, 1.18973149535723176e4932L);
 
     return (*env)->NewStringUTF(env, "Called functions with primitives (by value and by reference).");
 }
