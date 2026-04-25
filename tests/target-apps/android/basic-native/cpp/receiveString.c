a9b7fa4ec85dade23373dbeb39699d115a8cb32a
bernhste <s.bernhardsgruetter@proton.me>
2026-04-25 10:40:20 +0200

    simplified native string tests

--- tests/target-apps/android/basic-native/cpp/receiveString.c
+++ tests/target-apps/android/basic-native/cpp/receiveString.c
@@ -6,16 +6,13 @@ void receive_cstring(const char *s) {}
 
 void receive_utf8(const char *s) {}
 
-void receive_utf16(const wchar_t *s) {}
-
 JNIEXPORT jstring JNICALL
 Java_org_owasp_mastestapp_MastgTest_receiveStringsJNI(JNIEnv *env, jobject thiz)
 {
 
     // NUL-terminated variants
     receive_cstring("Hello, CString!");
-    receive_utf8("Hello, UTF-8! ❤️✅😭✨🫪🥹🔥✔️🫩");
-    receive_utf16(L"Hello, UTF-16! ❤️✅😭✨🫪🥹🔥✔️🫩");
+    receive_utf8("Hello, UTF-8! ❤️");
 
     return (*env)->NewStringUTF(env, "Called functions which receive C-Sting, UTF-8-String and UTF-16-String.");
 }
