79caadd130735ca9bff427213c560e1d01d5eb84
bernhste <s.bernhardsgruetter@proton.me>
2026-04-25 07:27:38 +0200

    updated native decoders

--- frooky/agent/shared/decoders/nativeDecoder.ts
+++ frooky/agent/shared/decoders/nativeDecoder.ts
@@ -1,15 +1,21 @@
 import type { NativeParam } from "../hook/nativeParameter";
 import type { BaseDecoder, DecodedValue } from "./baseDecoder";
-import { FallbackNativeDecoder, NativeFundamentalValueDecoder } from "./nativeBasicDecoder";
-import { FUNDAMENTAL_TYPES, normalizeNativeType } from "./nativeTypeNormalizer";
+import { NativeFallbackDecoder } from "./nativeFallbackDecoder";
+import { normalizeNativeType } from "./nativeTypeNormalizer";
+import { NativeValueDecoder } from "./nativeValueDecoder";
+
+export const FUNDAMENTAL_TYPES = ["void", "int", "uint", "long", "ulong", "char", "uchar", "size_t", "ssize_t", "float", "double", "int8", "uint8", "int16", "uint16", "int32", "uint32", "int64", "uint64", "bool"] as const;
+export type FundamentalType = (typeof FUNDAMENTAL_TYPES)[number];
 
 /*
  * This is the registry for native decoders
  */
 const nativeDecoderRegistry: Record<string, BaseDecoder<NativePointer, NativeParam>> = {
   // fundamental value decoders
-  ...Object.fromEntries(FUNDAMENTAL_TYPES.map((type) => [type, NativeFundamentalValueDecoder])),
-  // fundamental reference decoders
+  ...Object.fromEntries(FUNDAMENTAL_TYPES.map((type) => [type, NativeValueDecoder])),
+  // fundamental reference decoder
+  // pointer: NativeReferenceDecoder,
+  // other complex decoders
 };
 
 export const NativeDecoder: BaseDecoder<NativePointer, NativeParam> = {
@@ -23,7 +29,7 @@ export const NativeDecoder: BaseDecoder<NativePointer, NativeParam> = {
     // Resolve the decoder from the frooky parameter declaration and cache it
     const normalizedNativeType = normalizeNativeType(nativeParam);
     nativeParam.nativeType = normalizedNativeType;
-    nativeParam.decoder = nativeDecoderRegistry[normalizedNativeType.type] ?? FallbackNativeDecoder;
+    nativeParam.decoder = nativeDecoderRegistry[normalizedNativeType.type] ?? NativeFallbackDecoder;
 
     return nativeParam.decoder.decode(input, nativeParam, quickDecode);
   },
