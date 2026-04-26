4285f2ca1a6c2c278001180bbfa3f8587dc480aa
bernhste <s.bernhardsgruetter@proton.me>
2026-04-26 17:50:35 +0200

    cleanup comments

--- frooky/agent/android/hook/javaParam.ts
+++ frooky/agent/android/hook/javaParam.ts
@@ -5,10 +5,8 @@ import type { Param, ParamType } from "../../shared/hook/param";
  */
 export interface JavaParam extends Param {
   /**
-   * type of the actual implementation at runtime. this type can be different from the one declared
-   * if the declared one is an interface.
+   * type of the actual implementation at runtime. this type can be different from the one declared if the declared one is an interface.
    * If this parameter is set, the decoders will use this type to decode the values.
    */
   implementationType?: ParamType;
-  /** Optional human-readable name for the parameter, e.g. `"username"`. */
 }
