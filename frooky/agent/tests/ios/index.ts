7946e6a69c6cd312d4079e4bc83bf66fa82552d5
bernhste <s.bernhardsgruetter@proton.me>
2026-05-13 16:27:35 +0200

    adding frooky agent to ios tests

--- frooky/agent/tests/ios/index.ts
+++ frooky/agent/tests/ios/index.ts
@@ -1,3 +1,6 @@
+import { FrookyAgent } from "../../FrookyAgent.ts";
+import { JavaHookManager } from "../../android/hook/javaHookManager.ts";
+import { JavaHookValidator } from "../../android/hook/javaHookValidator.ts";
 import { runTests } from "../testFramework.ts";
 
 // The following test suite contains tests which test the test framework itself. It is disabled by default.
@@ -8,5 +11,6 @@ import "../../shared/index.test";
 import "./index.test.ts";
 
 setTimeout(() => {
+  globalThis.frooky = new FrookyAgent("Android", new JavaHookValidator(), new JavaHookManager(), 0, "device");
   runTests(send);
 }, 1000);
