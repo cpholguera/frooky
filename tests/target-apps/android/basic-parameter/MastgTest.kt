c2491f6cd0ab816b5b03accd06b4f0ff196c1475
bernhste <s.bernhardsgruetter@proton.me>
2026-06-19 15:10:27 +0200

    adding longer test longs for the array

--- tests/target-apps/android/basic-parameter/MastgTest.kt
+++ tests/target-apps/android/basic-parameter/MastgTest.kt
@@ -10,199 +10,210 @@ import java.math.BigInteger
 import java.security.KeyPairGenerator
 
 class MastgTest(private val context: Context) {
-    fun initRsaKeyPair(): String {
-        val keyPairGenerator =
-                KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore")
-
-        // Define what the key can be used for and how
-        val spec =
-                KeyGenParameterSpec.Builder(
-                                "TestKeyPair",
-                                KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY
+        fun initRsaKeyPair(): String {
+                val keyPairGenerator =
+                        KeyPairGenerator.getInstance(
+                                KeyProperties.KEY_ALGORITHM_RSA,
+                                "AndroidKeyStore"
                         )
-                        .setKeySize(2048)
-                        .setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
-                        .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
-                        .build()
 
-        keyPairGenerator.initialize(spec)
+                // Define what the key can be used for and how
+                val spec =
+                        KeyGenParameterSpec.Builder(
+                                        "TestKeyPair",
+                                        KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY
+                                )
+                                .setKeySize(2048)
+                                .setDigests(
+                                        KeyProperties.DIGEST_SHA256,
+                                        KeyProperties.DIGEST_SHA512
+                                )
+                                .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
+                                .build()
 
-        return "Initialized RAS Key Pair with Spec: ${spec.toString()}"
-    }
+                keyPairGenerator.initialize(spec)
 
-    fun buildIntentWithFlags(): String {
-        val securityFlags =
-                Intent.FLAG_GRANT_READ_URI_PERMISSION or
-                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION or
-                        Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION or
-                        Intent.FLAG_GRANT_PREFIX_URI_PERMISSION or
-                        Intent.FLAG_ACTIVITY_NEW_TASK or
-                        Intent.FLAG_ACTIVITY_CLEAR_TASK
-
-        Intent(Intent.ACTION_VIEW, Uri.EMPTY).apply {
-            setPackage("org.owasp.mastestapp")
-            setFlags(securityFlags)
+                return "Initialized RAS Key Pair with Spec: ${spec.toString()}"
         }
-        return "Intent with various flags sent. Binary masked integer is: $securityFlags"
-    }
 
-    // Single types
-    fun receiveString(arg: String) {}
-    fun receiveBoolean(arg: Boolean) {}
-    fun receiveByte(arg: Byte) {}
-    fun receiveShort(arg: Short) {}
-    fun receiveInt(arg: Int) {}
-    fun receiveLong(arg: Long) {}
-    fun receiveFloat(arg: Float) {}
-    fun receiveDouble(arg: Double) {}
-    fun receiveChar(arg: Char) {}
+        fun buildIntentWithFlags(): String {
+                val securityFlags =
+                        Intent.FLAG_GRANT_READ_URI_PERMISSION or
+                                Intent.FLAG_GRANT_WRITE_URI_PERMISSION or
+                                Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION or
+                                Intent.FLAG_GRANT_PREFIX_URI_PERMISSION or
+                                Intent.FLAG_ACTIVITY_NEW_TASK or
+                                Intent.FLAG_ACTIVITY_CLEAR_TASK
 
-    fun receiveBigInteger(arg: BigInteger) {}
-    fun receiveBigDecimal(arg: BigDecimal) {}
-    fun receiveList(arg: List<String>) {}
-    fun receiveMap(arg: Map<String, String>) {}
-    fun receiveSet(arg: Set<String>) {}
+                Intent(Intent.ACTION_VIEW, Uri.EMPTY).apply {
+                        setPackage("org.owasp.mastestapp")
+                        setFlags(securityFlags)
+                }
+                return "Intent with various flags sent. Binary masked integer is: $securityFlags"
+        }
 
-    enum class Direction {
-        NORTH,
-        SOUTH,
-        EAST,
-        WEST
-    }
-    fun receiveEnum(arg: Direction) {}
+        // Single types
+        fun receiveString(arg: String) {}
+        fun receiveBoolean(arg: Boolean) {}
+        fun receiveByte(arg: Byte) {}
+        fun receiveShort(arg: Short) {}
+        fun receiveInt(arg: Int) {}
+        fun receiveLong(arg: Long) {}
+        fun receiveFloat(arg: Float) {}
+        fun receiveDouble(arg: Double) {}
+        fun receiveChar(arg: Char) {}
 
-    // Arrays
-    fun receiveStringArray(arg: Array<String>) {}
-    fun receiveBooleanArray(arg: BooleanArray) {}
-    fun receiveByteArray(arg: ByteArray) {}
-    fun receiveShortArray(arg: ShortArray) {}
-    fun receiveIntArray(arg: IntArray) {}
-    fun receiveLongArray(arg: LongArray) {}
-    fun receiveFloatArray(arg: FloatArray) {}
-    fun receiveDoubleArray(arg: DoubleArray) {}
-    fun receiveCharArray(arg: CharArray) {}
-    fun receiveNestedObjectArray(
-            arg: Array<Array<Array<String>>>
-    ) {} // nested array of objects with depth of 3
-    fun receiveNestedPrimitivesArray(
-            arg: Array<Array<IntArray>>
-    ) {} // nested array of integer primitives with depth of 3
+        fun receiveBigInteger(arg: BigInteger) {}
+        fun receiveBigDecimal(arg: BigDecimal) {}
+        fun receiveList(arg: List<String>) {}
+        fun receiveMap(arg: Map<String, String>) {}
+        fun receiveSet(arg: Set<String>) {}
 
-    fun mastgTest(): String {
-        val r = DemoResults("basic-parameter")
+        enum class Direction {
+                NORTH,
+                SOUTH,
+                EAST,
+                WEST
+        }
+        fun receiveEnum(arg: Direction) {}
 
-        receiveString("Test String")
-        r.add(Status.PASS, "Test String")
+        // Arrays
+        fun receiveStringArray(arg: Array<String>) {}
+        fun receiveBooleanArray(arg: BooleanArray) {}
+        fun receiveByteArray(arg: ByteArray) {}
+        fun receiveShortArray(arg: ShortArray) {}
+        fun receiveIntArray(arg: IntArray) {}
+        fun receiveLongArray(arg: LongArray) {}
+        fun receiveFloatArray(arg: FloatArray) {}
+        fun receiveDoubleArray(arg: DoubleArray) {}
+        fun receiveCharArray(arg: CharArray) {}
+        fun receiveNestedObjectArray(
+                arg: Array<Array<Array<String>>>
+        ) {} // nested array of objects with depth of 3
+        fun receiveNestedPrimitivesArray(
+                arg: Array<Array<IntArray>>
+        ) {} // nested array of integer primitives with depth of 3
 
-        receiveBoolean(true)
-        r.add(Status.PASS, true.toString())
+        fun mastgTest(): String {
+                val r = DemoResults("basic-parameter")
 
-        receiveByte(127)
-        r.add(Status.PASS, 127.toString())
+                receiveString("Test String")
+                r.add(Status.PASS, "Test String")
 
-        receiveShort(32767)
-        r.add(Status.PASS, 32767.toString())
+                receiveBoolean(true)
+                r.add(Status.PASS, true.toString())
 
-        receiveInt(2147483647)
-        r.add(Status.PASS, 2147483647.toString())
+                receiveByte(127)
+                r.add(Status.PASS, 127.toString())
 
-        receiveLong(9223372036854775807L)
-        r.add(Status.PASS, 9223372036854775807L.toString())
+                receiveShort(32767)
+                r.add(Status.PASS, 32767.toString())
 
-        receiveFloat(3.14f)
-        r.add(Status.PASS, 3.14f.toString())
+                receiveInt(2147483647)
+                r.add(Status.PASS, 2147483647.toString())
 
-        receiveDouble(3.141592653589793)
-        r.add(Status.PASS, 3.141592653589793.toString())
+                receiveLong(9223372036854775807L)
+                r.add(Status.PASS, 9223372036854775807L.toString())
 
-        receiveChar('A')
-        r.add(Status.PASS, 'A'.toString())
+                receiveFloat(3.14f)
+                r.add(Status.PASS, 3.14f.toString())
 
-        val stringArray = arrayOf("a", "b", "c")
-        receiveStringArray(stringArray)
-        r.add(Status.PASS, stringArray.joinToString())
+                receiveDouble(3.141592653589793)
+                r.add(Status.PASS, 3.141592653589793.toString())
 
-        val boolArray = booleanArrayOf(true, false)
-        receiveBooleanArray(boolArray)
-        r.add(Status.PASS, boolArray.joinToString())
+                receiveChar('A')
+                r.add(Status.PASS, 'A'.toString())
 
-        val byteArray = byteArrayOf(1, 2, 3)
-        receiveByteArray(byteArray)
-        r.add(Status.PASS, byteArray.joinToString())
+                val stringArray = arrayOf("a", "b", "c")
+                receiveStringArray(stringArray)
+                r.add(Status.PASS, stringArray.joinToString())
 
-        val shortArray = shortArrayOf(1, 2, 3)
-        receiveShortArray(shortArray)
-        r.add(Status.PASS, shortArray.joinToString())
+                val boolArray = booleanArrayOf(true, false)
+                receiveBooleanArray(boolArray)
+                r.add(Status.PASS, boolArray.joinToString())
 
-        val intArray = intArrayOf(1, 2, 3)
-        receiveIntArray(intArray)
-        r.add(Status.PASS, intArray.joinToString())
+                val byteArray = byteArrayOf(1, 2, 3)
+                receiveByteArray(byteArray)
+                r.add(Status.PASS, byteArray.joinToString())
 
-        val longArray = longArrayOf(1L, 2L, 3L)
-        receiveLongArray(longArray)
-        r.add(Status.PASS, longArray.joinToString())
+                val shortArray = shortArrayOf(1, 2, 3)
+                receiveShortArray(shortArray)
+                r.add(Status.PASS, shortArray.joinToString())
 
-        val floatArray = floatArrayOf(1.1f, 2.2f)
-        receiveFloatArray(floatArray)
-        r.add(Status.PASS, floatArray.joinToString())
+                val intArray = intArrayOf(1, 2, 3)
+                receiveIntArray(intArray)
+                r.add(Status.PASS, intArray.joinToString())
 
-        val doubleArray = doubleArrayOf(1.1, 2.2)
-        receiveDoubleArray(doubleArray)
-        r.add(Status.PASS, doubleArray.joinToString())
+                val longArray =
+                        longArrayOf(
+                                9223372036854775805L,
+                                9223372036854775806L,
+                                9223372036854775807L
+                        )
+                receiveLongArray(longArray)
+                r.add(Status.PASS, longArray.joinToString())
 
-        val charArray = charArrayOf('x', 'y', 'z')
-        receiveCharArray(charArray)
-        r.add(Status.PASS, charArray.joinToString())
+                val floatArray = floatArrayOf(1.1f, 2.2f)
+                receiveFloatArray(floatArray)
+                r.add(Status.PASS, floatArray.joinToString())
 
-        val bigInt = BigInteger("123456789012345678901234567890")
-        receiveBigInteger(bigInt)
-        r.add(Status.PASS, bigInt.toString())
+                val doubleArray = doubleArrayOf(1.1, 2.2)
+                receiveDoubleArray(doubleArray)
+                r.add(Status.PASS, doubleArray.joinToString())
 
-        val bigDec = BigDecimal("3.141592653589793238462643383")
-        receiveBigDecimal(bigDec)
-        r.add(Status.PASS, bigDec.toString())
+                val charArray = charArrayOf('x', 'y', 'z')
+                receiveCharArray(charArray)
+                r.add(Status.PASS, charArray.joinToString())
 
-        val list = listOf("a", "b", "c")
-        receiveList(list)
-        r.add(Status.PASS, list.toString())
+                val bigInt = BigInteger("123456789012345678901234567890")
+                receiveBigInteger(bigInt)
+                r.add(Status.PASS, bigInt.toString())
 
-        val map = mapOf("key" to "value")
-        receiveMap(map)
-        r.add(Status.PASS, map.toString())
+                val bigDec = BigDecimal("3.141592653589793238462643383")
+                receiveBigDecimal(bigDec)
+                r.add(Status.PASS, bigDec.toString())
 
-        val set = setOf("a", "b", "c")
-        receiveSet(set)
-        r.add(Status.PASS, set.toString())
+                val list = listOf("a", "b", "c")
+                receiveList(list)
+                r.add(Status.PASS, list.toString())
 
-        val nestedObjects =
-                arrayOf(
-                        arrayOf(arrayOf("a", "b", "c"), arrayOf("d", "e", "f")),
-                        arrayOf(arrayOf("g", "h", "i"), arrayOf("j", "k", "l"))
-                )
+                val map = mapOf("key" to "value")
+                receiveMap(map)
+                r.add(Status.PASS, map.toString())
 
-        receiveNestedObjectArray(nestedObjects)
-        r.add(Status.PASS, nestedObjects.toString())
+                val set = setOf("a", "b", "c")
+                receiveSet(set)
+                r.add(Status.PASS, set.toString())
 
-        val nestedPrimitives =
-                arrayOf(
-                        arrayOf(intArrayOf(1, 2, 3), intArrayOf(4, 5, 6)),
-                        arrayOf(intArrayOf(7, 8, 9), intArrayOf(10, 11, 12))
-                )
+                val nestedObjects =
+                        arrayOf(
+                                arrayOf(arrayOf("a", "b", "c"), arrayOf("d", "e", "f")),
+                                arrayOf(arrayOf("g", "h", "i"), arrayOf("j", "k", "l"))
+                        )
 
-        receiveNestedPrimitivesArray(nestedPrimitives)
-        r.add(Status.PASS, nestedPrimitives.toString())
+                receiveNestedObjectArray(nestedObjects)
+                r.add(Status.PASS, nestedObjects.toString())
 
-        val longIntegerArray = Array(100) { it + 1 }.toIntArray()
-        receiveIntArray(longIntegerArray)
-        r.add(Status.PASS, longIntegerArray.toString())
+                val nestedPrimitives =
+                        arrayOf(
+                                arrayOf(intArrayOf(1, 2, 3), intArrayOf(4, 5, 6)),
+                                arrayOf(intArrayOf(7, 8, 9), intArrayOf(10, 11, 12))
+                        )
 
-        receiveEnum(Direction.NORTH)
-        r.add(Status.PASS, Direction.NORTH.toString())
+                receiveNestedPrimitivesArray(nestedPrimitives)
+                r.add(Status.PASS, nestedPrimitives.toString())
 
-        r.add(Status.PASS, this.buildIntentWithFlags())
+                val longIntegerArray = Array(100) { it + 1 }.toIntArray()
+                receiveIntArray(longIntegerArray)
+                r.add(Status.PASS, longIntegerArray.toString())
 
-        r.add(Status.PASS, initRsaKeyPair())
+                receiveEnum(Direction.NORTH)
+                r.add(Status.PASS, Direction.NORTH.toString())
 
-        return r.toJson()
-    }
+                r.add(Status.PASS, this.buildIntentWithFlags())
+
+                r.add(Status.PASS, initRsaKeyPair())
+
+                return r.toJson()
+        }
 }
